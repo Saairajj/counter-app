ecfaadbeff9cbf53f01436b1179449ea41dd7939
Sairaj Jadhav <saairajj812@gmail.com>
2021-03-10 21:21:25 +0530

    Setting up for new project

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
+import 'font-awesome/css/font-awesome.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Counter />
-  </React.StrictMode>,
+  
+    <App />
+  ,
   document.getElementById('root')
 );
 
